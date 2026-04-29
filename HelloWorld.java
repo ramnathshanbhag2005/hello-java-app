de792c81d0fc53d533afebec78f4c834ca5fdfe1
Ramnath Shanbhag J <jramnathshanbhag@gmail.com>
2026-04-29 20:44:05 +0530

    added Jenkins pipeline

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("Checking automation again 🚀");
+      System.out.println("Checking automate again 🚀");
     }
 }
